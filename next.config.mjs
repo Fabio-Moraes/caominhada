4ef033dd82d5df2148a4e0a826e66c588c31a1e4
Fabio-Moraes <fabio95x@gmail.com>
2024-06-09 19:09:02 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ['legpzytiaeepvlgmekfk.supabase.co'],
+  },};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,79 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
+  const baseUrl =
+    'https://legpzytiaeepvlgmekfk.supabase.co/storage/v1/object/public/caominhada/';
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>2ª CÃOMINHADA 14/07/2024</p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={styles.emblem}
+                alt="description of image"
+                src={baseUrl.concat('teodoro_sampaio.png')}
+              />
+            </a>
+          </div>
+        </div>
+        <div className={styles.center}>
+          <img
+            alt="description of image"
+            style={{ width: '100vw', height: 'auto' }}
+            src={baseUrl.concat('dogs-swalk.png')}
+          />
+          <br />
+          <h1>Equipe vigilância</h1>
+          <br />
+          <img
+            alt="description of image"
+            style={{ width: '100vw', height: 'auto' }}
+            src={baseUrl.concat('equipe.png')}
+          />
+        </div>
+
+        <div className={styles.grid}>
+          <a className={styles.card} target="_blank" rel="noopener noreferrer">
+            <h2>2ª Cãominhada de Teodoro Sampaio</h2>
+            <p>
+              Venha participar da nossa segunda Cãominhada! Traga seu amigo de quatro patas
+              para um dia de diversão e conscientização. Juntos, vamos promover a saúde,
+              bem-estar e o amor pelos animais. Não perca essa oportunidade de se divertir e
+              fazer novos amigos!
+            </p>
+          </a>
+
+          <a className={styles.card} target="_blank" rel="noopener noreferrer">
+            <h2>Pedidos de camisetas</h2>
+            <p>Valor 38,00 (preço da fábrica MSA Serigrafia- Pirapozinho/SP)</p>
+            <p>Pedido até 31/05 Pagamento até 31/05 Entrega 25/06</p>
+            <br />
+            <p>Pagamento PIX</p>
+            <p>33321788810</p>
+            <p>Cristiane Oliveira Andrade</p>
+          </a>
+
+          <a className={styles.card} target="_blank" rel="noopener noreferrer">
+            <img
+              alt="description of image"
+              style={{
+                width: '100%',
+                height: 'auto',
+                borderRadius: '10px'
+              }}
+              src={baseUrl.concat('camisetas.png')}
             />
           </a>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <footer className={styles.footer}>© 2024 Desenvolvido por Fabio Moraes</footer>
+      </main>
+    </>
   );
 }
